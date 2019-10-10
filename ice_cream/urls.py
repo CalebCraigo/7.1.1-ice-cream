437f613ce47929fc1b27b5bf24ce4f96d9949dab
CalebCraigo <CalebDOS1@yahoo.com>
2019-10-10 11:57:59 -0400

    updated auth functionality

--- ice_cream/urls.py
+++ ice_cream/urls.py
@@ -5,7 +5,7 @@ app_name = 'ice_cream'
 
 urlpatterns = [
 
-    path(r'^delete/(?P<pk>[0-9]+)/$', views.icecream_delete, name='icecream_delete'),
+    # path(r'^delete/(?P<pk>[0-9]+)/$', views.icecream_delete, name='icecream_delete'),
     path('new/', views.CreateView.as_view(), name='create'),
     path('<int:ice_cream_id>/results/', views.results, name='results'),
     path('<int:ice_cream_id>/vote/', views.vote, name='vote'),
